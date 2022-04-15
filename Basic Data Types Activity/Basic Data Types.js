d07b3b3192cba45dd3ec8dd02f3afca4b0d208fd
SamSkish <s4mschroeder@gmail.com>
2022-04-14 20:01:59 -0500

    adds Basic Data Types

---
+++ Basic Data Types Activity/Basic Data Types.js
@@ -0,0 +1,22 @@
+let greetings = "Finally your here";
+let name = 'Taichi';
+let title = 'Telekinetic Prodigy';
+let worldName = 'New Era';
+
+let age = 15;
+let year = 3016;
+let millennium = 4;
+
+let abilitys = ['Telekinesis', 'Absorption', 'Brainwashing', 'Pyrokinetics'];
+
+let onlyPersonWithPowers = false;
+
+console.log(greetings + " hi I'm " + name + ".");
+console.log("They call me the" + title);
+console.log("I have that title because I was born right in the beginning of the" + worldName);
+console.log("Because of that I gained special powers, dispite being only " + age + " years old.");
+console.log("They year is " + year + "Early into the " + millennium + "th millennium" + "\n");
+console.log("You look confused, do you have a question?");
+console.log("Me: Are you the only person with powers?");
+console.log("Taichi: " + onlyPersonWithPowers + ", but i'm the strongest " + abilitys[0] + " user.");
+console.log("Well I have to go unfortunetly, it was a pleasure meeting you.");
